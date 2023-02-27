01264532dddcc2f8c002fa7132c22381baa27cfd
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-02-27 22:53:46 +0000

    Added home, about-us, contact components
    added route bahaviour (unfinished)

--- studdy-buddy/src/app/app-routing.module.ts
+++ studdy-buddy/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import pages
+import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  /* Add route info to route array*/
+  { path: '' component:HomeComponent}
+  { path: 'contact' component:ContactComponent}
+  { path: 'app-component', component:AppComponent }, /* Not sure if this one is needed since it is the basic component*/
+  { path: 'about-us-component', component:AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
