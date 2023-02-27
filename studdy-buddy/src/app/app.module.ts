9b584b817b8777ea4f293abe7109c05e57a6df37
alexander.schranner <alexader.schranner@rwth-aachen.de>
2023-02-27 22:49:57 +0000

    Add icons to generated side bar component

--- studdy-buddy/src/app/app.module.ts
+++ studdy-buddy/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
