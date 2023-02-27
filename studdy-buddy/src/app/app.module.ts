61dbb7be7dc73fdeba35c79030ad44363b5b51b4
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-02-27 22:53:54 +0000

    Added components and routes

--- studdy-buddy/src/app/app.module.ts
+++ studdy-buddy/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    AboutUsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
