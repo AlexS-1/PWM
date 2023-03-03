742cdbf708abdfa424bcd352a58e01c5b6153073
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-03-03 13:00:44 +0000

    Added linking from log-in page
    links added towards password reset and create account page

--- studdy-buddy/src/app/app-routing.module.ts
+++ studdy-buddy/src/app/app-routing.module.ts
@@ -1,9 +1,4 @@
-import { LogInComponent } from './log-in/log-in.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
-import { CreateCourseComponent } from './create-course/create-course.component';
-import { ListCoursesComponent } from './list-courses/list-courses.component';
-import { BrowseCoursesComponent } from './browse-courses/browse-courses.component';
-import { MyAccountComponent } from './my-account/my-account.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -12,20 +7,28 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
+import { ListCoursesComponent } from './list-courses/list-courses.component';
+import { BrowseCoursesComponent } from './browse-courses/browse-courses.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
 const routes: Routes = [
   /* Add route info to route array*/
   { path: '', component:HomeComponent},
-  { path: 'contact-component', component:ContactComponent},
-  { path: 'app-component', component:AppComponent }, /* Not sure if this one is needed since it is the basic component*/
-  { path: 'about-us-component', component:AboutUsComponent },
-  { path: 'home-component', component:HomeComponent},
-  { path: 'my-account-component', component:MyAccountComponent},
-  { path: 'browse-courses-component', component:BrowseCoursesComponent},
-  { path: 'list-courses-component', component:ListCoursesComponent},
-  { path: 'create-course-component', component:CreateCourseComponent},
-  { path: 'create-account-component', component:CreateAccountComponent},
-  { path: 'log-in-component', component:LogInComponent}
+  { path: 'contact', component:ContactComponent},
+  { path: 'app', component:AppComponent }, /* Not sure if this one is needed since it is the basic component*/
+  { path: 'about-us', component:AboutUsComponent },
+  { path: 'home', component:HomeComponent},
+  { path: 'my-account', component:MyAccountComponent},
+  { path: 'browse-courses', component:BrowseCoursesComponent},
+  { path: 'list-courses', component:ListCoursesComponent},
+  { path: 'log-in', component:LogInComponent},
+    { path: 'log-in/forgot-password', component:ForgotPasswordComponent},
+    { path: 'log-in/create-account', component:CreateAccountComponent},
+  { path: 'create-course', component:CreateCourseComponent},
+  { path: 'create-account', component:CreateAccountComponent}
 ];
 
 @NgModule({
