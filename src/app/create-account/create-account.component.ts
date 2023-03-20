1874ea8f8864e49ffde60470c701fc702463cd40
alexander.schranner <alexader.schranner@rwth-aachen.de>
2023-03-20 23:17:46 +0000

    Regex for password in create-account

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class CreateAccountComponent {
 
+  checkValidityPassword(passwordStirng: any) {
+    var inputValue = passwordStirng.target.value;
+     if (inputValue.match("(?=.*?[0-9])") == null) {
+      console.log("Must contain a number");
+    } else if (inputValue.match("(?=.*?[#?!@$%^&*-])") == null) {
+      console.log("Must contain special character");
+    } else if (inputValue.match("(?=.*?[A-Z])") == null) {
+      console.log("Must contain a uppercase letter");
+    } else if (inputValue.match("(?=.*?[a-z])") == null) {
+      console.log("Must contain a lowercase letter");
+    } else if (inputValue.match(".{8,}") == null) {
+      console.log("Must be minimum 8 characters");
+    }
+    /*var String pswValue = this.password.nativeElement.
+    if (this.password.value != this.repeatedPassword.value) {
+      console.log("Must match");
+    }*/
+  }
 }
