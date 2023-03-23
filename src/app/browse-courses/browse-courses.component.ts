67325a061a52637a28d6db2d1dbd35a5f0f698a1
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-03-23 22:55:33 +0000

    Reverted all changes to browse-courses

--- src/app/browse-courses/browse-courses.component.ts
+++ src/app/browse-courses/browse-courses.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AppFetchDataTsService } from './../app.fetch-data.ts.service';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-browse-courses',
   templateUrl: './browse-courses.component.html',
-  styleUrls: ['./browse-courses.component.css'],
-  template: `
-  <a href="course-details" routerLink="/course-details" routerLinkActive="active" ariaCurrentWhenActive="page" class="card">
-      <div class="inner-box">
-
-      </div>
-      <div class="description" *ngFor="let item of data">
-        {{item}}
-      </div>
-    </a>
-  `
+  styleUrls: ['./browse-courses.component.css']
 })
-export class BrowseCoursesComponent implements OnInit {
+export class BrowseCoursesComponent {
 
-  data:any;
-
-  constructor(private fetchData:AppFetchDataTsService) {}
-
-  ngOnInit(): void {
-    console.log('reached OnInit')
-    this.fetchData.getContentData().subscribe((data) => {
-      this.data = data;
-    });
-    console.log('Loaded Data: ' + this.data);
-  }  
 }
