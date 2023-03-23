a6a7b8f6db18bf166aa5cb140381686d75fe4059
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-03-23 22:05:52 +0000

    Modified browse-course component
    Reworked to dynamically load in course cards from json
    
    Not working correctly

--- src/app/browse-courses/browse-courses.component.ts
+++ src/app/browse-courses/browse-courses.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppFetchDataTsService } from './../app.fetch-data.ts.service';
+
 
 @Component({
   selector: 'app-browse-courses',
   templateUrl: './browse-courses.component.html',
-  styleUrls: ['./browse-courses.component.css']
-})
-export class BrowseCoursesComponent {
+  styleUrls: ['./browse-courses.component.css'],
+  template: `
+  <a href="course-details" routerLink="/course-details" routerLinkActive="active" ariaCurrentWhenActive="page" class="card">
+      <div class="inner-box">
 
+      </div>
+      <div class="description" *ngFor="let item of data">
+        {{item}}
+      </div>
+    </a>
+  `
+})
+export class BrowseCoursesComponent implements OnInit {
+
+  data:any;
+
+  constructor(private fetchData:AppFetchDataTsService) {}
+
+  ngOnInit(): void {
+    console.log('reached OnInit')
+    this.fetchData.getContentData().subscribe((data) => {
+      this.data = data;
+    });
+    console.log('Loaded Data: ' + this.data);
+  }  
 }
