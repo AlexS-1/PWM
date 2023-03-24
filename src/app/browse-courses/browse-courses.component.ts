080ce3ff24a1570e3cf57398a43b391f062000bc
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-03-24 01:33:20 +0000

    Modified browse-component, course.json
    Added dynamical loading of cours cards form json file
    Added content to json file

--- src/app/browse-courses/browse-courses.component.ts
+++ src/app/browse-courses/browse-courses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-browse-courses',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class BrowseCoursesComponent {
 
+}*/
+import { Component, OnInit } from '@angular/core';
+import { AppFetchDataTsService } from './../app.fetch-data.ts.service';
+
+@Component({
+  selector: 'app-browse-courses',
+  templateUrl: './browse-courses.component.html',
+  styleUrls: ['./browse-courses.component.css']
+})
+export class BrowseCoursesComponent implements OnInit {
+  contentData:any;
+
+  constructor(private fetchData:AppFetchDataTsService) {}
+
+  ngOnInit(): void {
+    this.fetchData.getContentData().subscribe((data) => {
+      this.contentData = data;
+    });
+  }
 }
