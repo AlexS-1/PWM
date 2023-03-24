74c3961fef77b7ee39ecbefe1b50f8da5cf00b33
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-03-24 01:31:57 +0000

    Modified test component
    use content loading from json to display html content

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppFetchDataTsService } from './../app.fetch-data.ts.service';
 
 @Component({
   selector: 'app-test',
+
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
+  contentData:any;
 
+  constructor(private fetchData:AppFetchDataTsService) {}
+
+  ngOnInit(): void {
+    this.fetchData.getContentData().subscribe((data) => {
+      this.contentData = data;
+    });
+  }
 }
+
+
