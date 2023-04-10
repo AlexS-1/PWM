c7ad62e6bac5acc656837d95320b3a737862f317
karl.kroneberg <karl.kroneberg@s2021.tu-chemnitz.de>
2023-04-10 14:33:47 +0100

    connected same json file for my courses too

--- src/app/my-courses/my-courses.component.ts
+++ src/app/my-courses/my-courses.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jsonData from './../../assets/content/course.json';
 
 @Component({
   selector: 'app-my-courses',
   templateUrl: './my-courses.component.html',
   styleUrls: ['./my-courses.component.css']
 })
-export class MyCoursesComponent {
+export class MyCoursesComponent implements OnInit {
+  courses: any[] = [];
+  selectedCourseIds = [1, 2]; // Liste der ausgewählten Kurs-IDs
 
+  ngOnInit(): void {
+    this.loadSelectedCourses();
+  }
+
+  private loadSelectedCourses(): void {
+    this.courses = jsonData.filter(course => this.selectedCourseIds.includes(course.id));
+  }
 }
