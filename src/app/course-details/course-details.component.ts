d632c5b350ab1e74a85fc6ae2bd5368f561681e0
karl.kroneberg <karl.kroneberg@s2021.tu-chemnitz.de>
2023-04-11 14:10:48 +0100

    complete content

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import jsonData from './../../assets/content/course.json';
+import commentData from './../../assets/content/comments.json';
 
 interface DataEntry {
   id: number;
   title: string;
   description: string;
   userId: string;
+}
+
+interface CommentEntry {
+  username: string;
+  review: string;
+  kurs_id: number;
+  date: string;
 }
 
 @Component({
@@ -16,6 +24,7 @@ interface DataEntry {
 })
 export class CourseDetailsComponent implements OnInit {
   dataEntry: DataEntry | undefined;
+  reviews: CommentEntry[] = [];
 
   constructor(private route: ActivatedRoute) {}
 
@@ -23,6 +32,7 @@ export class CourseDetailsComponent implements OnInit {
     this.route.params.subscribe((params) => {
       const id = params['id'];
       this.dataEntry = jsonData.find((entry) => entry.id === parseInt(id, 10));
+      this.reviews = commentData.filter((entry) => entry.kurs_id === parseInt(id, 10));
     });
   }
 }
