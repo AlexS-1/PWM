79b2c6c6aa319a41b9d4714ab605cfd91bbdf9a9
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-04-16 16:01:52 +0100

    Modified Test component
    Did testing with double binding in angular

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,22 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { AppFetchDataTsService } from './../app.fetch-data.ts.service';
+import { AuthService } from './../auth-service.service';
+import { Component } from '@angular/core';
+
+
+  /*@Component({
+    selector: 'app-test',
+
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.css']
+  })*/
 
 @Component({
-  selector: 'app-test',
-
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-login',
+  template: `
+<div class="signup-form">
+  <h2>Welcome !</h2>
+  <div>{{email}}</div>
+  <div>{{password}}</div>
+  <form (submit)="onSubmit()">
+    <div class="form-group">
+      <input type="email" class="form-control" placeholder="Email" [(ngModel)]="email" name="email" required>
+    </div>
+    <div class="form-group">
+      <input type="password" class="form-control" placeholder="Password" [(ngModel)]="password" required>
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block">Login</button>
+    </div>
+    <div class="form-group text-center">
+      <a href="forgot-password" routerLink="/forgot-password" routerLinkActive="active" ariaCurrentWhenActive="page">Forgot Password?</a>
+      <a href="create-account" routerLink="/create-account" routerLinkActive="active" ariaCurrentWhenActive="page">Don't have an Account yet?</a>
+    </div>
+  </form>
+</div>
+    <form (submit)="onSubmit()">
+      <label>
+        Username:
+        <input type="email" [(ngModel)]="username" name="username" required/>
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" [(ngModel)]="password" name="password" required/>
+      </label>
+      <br />
+      <button type="submit">Log In</button>
+    </form>
+    <div>{{username}}</div>
+    <div>{{password}}</div>
+  `,
 })
-export class TestComponent implements OnInit {
-  contentData:any;
+export class TestComponent {
+  email: string = 'e';
+  password: string = 'p1';
+  password2: string = 'p2';
+  username: string = 'u';
 
-  constructor(private fetchData:AppFetchDataTsService) {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
-    this.fetchData.getContentData().subscribe((data) => {
-      this.contentData = data;
-    });
+  onSubmit() {
+    const isValid = this.authService.validateCredentials(this.username, this.password);
+    if (isValid) {
+      // perform login
+    } else {
+      // display error message
+    }
   }
 }
-
 
