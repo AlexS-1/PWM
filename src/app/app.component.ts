6c9f923ce5152a49ea6ea91359ea55382a8c18a2
alexander.schranner <alexader.schranner@rwth-aachen.de>
2023-04-24 22:54:59 +0100

    Fix hiding footer for chrome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NavBarService } from './nav-bar.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  constructor(private navBarService: NavBarService){}
+
+  declare sideBarOff: boolean;
+
+  ngOnInit() {
+    this.navBarService.isOpen$.subscribe(isOpen => this.sideBarOff = isOpen);
+  }
+  
   title = 'studdy-buddy';
+
 }
