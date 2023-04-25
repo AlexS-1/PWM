3f8999509658f81b1b9bd3aaf7f9d2752bc4b7a0
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-04-25 19:12:40 +0100

    V1 profile picture
    Needs further work

--- src/app/feature/my-account/my-account.component.ts
+++ src/app/feature/my-account/my-account.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.css']
 })
+
 export class MyAccountComponent {
+
+  declare imageUrl: string;
+  
+  ngOninit(){
+    this.imageUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+  }
 
 }
