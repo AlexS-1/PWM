e81422201ce34121c67e74d949b3a9db88dd1ba1
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-04-25 20:42:36 +0100

    Added test case in "test cmponent"

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,69 +1,26 @@
-import { AuthService } from './../core/auth-service.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
-  /*@Component({
+  @Component({
     selector: 'app-test',
 
     templateUrl: './test.component.html',
     styleUrls: ['./test.component.css']
-  })*/
+  })
 
-@Component({
-  selector: 'app-login',
-  template: `
-<div class="signup-form">
-  <h2>Welcome !</h2>
-  <div>{{email}}</div>
-  <div>{{password}}</div>
-  <form (submit)="onSubmit()">
-    <div class="form-group">
-      <input type="email" class="form-control" placeholder="Email" [(ngModel)]="email" name="email" required>
-    </div>
-    <div class="form-group">
-      <input type="password" class="form-control" placeholder="Password" [(ngModel)]="password" required>
-    </div>
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary btn-block">Login</button>
-    </div>
-    <div class="form-group text-center">
-      <a href="forgot-password" routerLink="/forgot-password" routerLinkActive="active" ariaCurrentWhenActive="page">Forgot Password?</a>
-      <a href="create-account" routerLink="/create-account" routerLinkActive="active" ariaCurrentWhenActive="page">Don't have an Account yet?</a>
-    </div>
-  </form>
-</div>
-    <form (submit)="onSubmit()">
-      <label>
-        Username:
-        <input type="email" [(ngModel)]="username" name="username" required/>
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" [(ngModel)]="password" name="password" required/>
-      </label>
-      <br />
-      <button type="submit">Log In</button>
-    </form>
-    <div>{{username}}</div>
-    <div>{{password}}</div>
-  `,
-})
+
 export class TestComponent {
-  email: string = 'e';
-  password: string = 'p1';
-  password2: string = 'p2';
-  username: string = 'u';
-
-  constructor(private authService: AuthService) {}
-
-  onSubmit() {
-    const isValid = this.authService.login(this.username, this.password);
-    if (isValid) {
-      // perform login
-    } else {
-      // display error message
-    }
+  items: Observable<any[]>;
+  users: Observable<any[]>;
+  
+  constructor(db: AngularFirestore){
+    this.items = db.collection('0').valueChanges();
+    this.users = db.collection('1').valueChanges();
   }
+
+  
+    
 }
 
