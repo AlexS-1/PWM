40a30deb148deff10314453e12ca3ee7365884f3
Karl Deilmann <karl.christian.deilmann@rwth-aachen.de>
2023-05-01 22:00:47 +0100

    Changed logged-in check to use firebase

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -11,14 +11,8 @@ export class AuthGuardService {
   constructor(private authService: AuthService, private router: Router) {}
 
   // Check wherether a user has the required role to access a route
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    const roles = route.data['roles'] as Array<string>;
-    const userRole = this.authService.getUserRole();
-
-    // console.log(roles);   // for debugging
-    // console.log(userRole);// for debugging
-
-    if (!this.authService.isLoggedIn() || !roles.includes(userRole)) {
+  async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
+    if (! await this.authService.isLoggedIn()) {
       // redirect to the login page if not logged in
       this.router.navigate(['/log-in']);
       return false;     // State logged out
--- src/app/core/auth-service.service.ts
+++ src/app/core/auth-service.service.ts
@@ -1,54 +1,16 @@
 import { BackendDataService } from 'src/app/core/backend-data.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getDocs, query, where } from 'firebase/firestore';
+import { getDocs } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  // Variable to hold userdata arry
-  private users: { id: number; email: string; password: string }[] = [];
 
-  // variables to hold login status and role
-  private loggedIn = false;
-  private userRole = 'visitor';
+  constructor(private router: Router, private firestore: AngularFirestore, private backendDataService: BackendDataService) { }
 
-  constructor(private http: HttpClient, private router: Router, private firestore: AngularFirestore, private backendDataService: BackendDataService) {
-    // DEPRICATED: Use Firestor database
-    // Perform loading of user data (only needed for json method)
-    this.loadUsers();
-
-    // Initialize login status vairables
-    this.loggedIn = !!sessionStorage.getItem('loggedInToken');
-
-    ////////////////
-    // ATTENTION: // This will read the local chached last state. For testing make sure to be logged out befor testing anything
-    ////////////////
-    this.userRole = sessionStorage.getItem('role') || 'visitor';      // ToDo: add verification to set logged in user state if token is still valid
-  }
-
-  /////////////////////////////////
-  /// Validation of credentials ///
-  /////////////////////////////////
-
-  // DEPRICATED: Use Firestor database
-  // load user credential data from json file
-  loadUsers() {
-    return this.http.get<{ users: { id: number; email: string; password: string }[] }>('../assets/userdata/userLogInData.json').subscribe((data) => {
-      this.users = data.users;
-      //console.log(data);            // for debugging
-      //console.log(this.users[0]);   // for debugging
-    });
-  }
-
-  // DEPRICATED: Use Firestor database
-  // Helper function to check entries in users[] loaded from database
-  private findUserByMailAndPw(userEntry : { id: number; email: string; password: string }, email: string, password: string){
-    return userEntry.email === email && userEntry.password === password ;
-  }
 
   // Valifation function to be called by login function
   // Returns the first match for email+password pair in user data array
@@ -86,9 +48,7 @@ export class AuthService {
   
     if(succssfulLookup){
       let token = sessionStorage.getItem('logInToken');
-      console.log('token: ', token);      // for debugging
       const tokenDoc = await this.backendDataService.getloggedInData(token);
-      console.log('tokenDoc: ', tokenDoc);      // for debugging
       if(tokenDoc != null){
         if(!tokenDoc['exists']()){
           let newEntryDoc = await db.collection('loggedIn').add(data);
@@ -118,24 +78,21 @@ export class AuthService {
   // Function to perfrorm logout and clear session storage
   // Sets login state to false, removes user role
   logout() : boolean{
-    this.loggedIn = false;
-    this.userRole = 'visitor';
     this.backendDataService.removeloggedInData(sessionStorage.getItem('logInToken'));
     sessionStorage.removeItem('logInToken');
-    console.log("Successfully logged out")  // for debugging
     return true;
   }
 
   // Getter for login status, logged in = true
-  isLoggedIn() : boolean{
-    return this.loggedIn || !!sessionStorage.getItem('loggedInToken');   // for debugging;
-  }
-
-  // Getter for user role
-  // 'visitor' = not loged in, 'user' = logged in user, 'admin'= logged in user with admin rights
-  getUserRole() : string{
-    this.userRole = sessionStorage.getItem('role') as string;
-    return this.userRole;
+  async isLoggedIn() : Promise<boolean>{
+    let token = sessionStorage.getItem('logInToken');
+    const tokenDoc = await this.backendDataService.getloggedInData(token);
+    if(tokenDoc != null){
+      if(tokenDoc['id'] === token){
+        return true
+      }
+    }
+    return false;
   }
 }
 
